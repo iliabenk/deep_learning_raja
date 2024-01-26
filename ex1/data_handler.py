Changed LeNet-5 model, fixed normalization bug and moved code to a better location

ex1/data_handler.py
@@ -1,12 +1,15 @@
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from utils import mnist_reader
+from configs import DATA_DIR
+import torchvision.transforms as transforms
 
 class Lenet5_Dataset(Dataset):
     def __init__(self, X, y, transform=None):
         assert len(y) == X.shape[0], (f"len(y)={len(y)}, X.shape={X.shape}, which do not match.\n"
                                       f"Must maintain len(y) == X.shape[0]")
 
-        self.X = np.reshape(X, (-1, 1, 28, 28))
+        self.X = np.reshape(X, (-1, 28, 28, 1))
         self.y = y
         self.transform = transform
 
@@ -21,3 +24,39 @@ class Lenet5_Dataset(Dataset):
             image = self.transform(image)
 
         return image, label
+
+
+def load_data(batch_size, transform_func=None):
+    X_train, y_train = mnist_reader.load_mnist(DATA_DIR, kind='train')
+    X_test, y_test = mnist_reader.load_mnist(DATA_DIR, kind='t10k')
+
+    if transform_func:
+        transform = transform_func()
+    else:
+        transform = None
+
+    train_loader = DataLoader(dataset=Lenet5_Dataset(X_train, y_train, transform=transform),
+                              batch_size=batch_size,
+                              shuffle=True)
+
+    test_loader = DataLoader(dataset=Lenet5_Dataset(X_test, y_test, transform=transform),
+                              batch_size=batch_size,
+                              shuffle=False)
+
+    return train_loader, test_loader
+
+def normalize(x):
+    mu = 0.2860 # Calculated on X_train
+    sigma = 0.3530 # Calculated on X_train
+
+    x = transforms.Normalize(mean=mu, std=sigma)(x)
+
+    return x
+
+def transform_func():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(normalize)
+    ])
+
+    return transform

ex1/main.py
@@ -7,51 +7,10 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import os
 
-from configs import DATA_DIR, MODEL_TYPES, INIT_SEED, TENSORBOARD_DIR, MODELS_OUTPUT_DIR
-from utils import mnist_reader, seed_handler, model_utils
+from configs import MODEL_TYPES, INIT_SEED, TENSORBOARD_DIR, MODELS_OUTPUT_DIR
+from utils import seed_handler, model_utils
 from model import Lenet5
-from data_handler import Lenet5_Dataset
-
-def _load_data(batch_size, transform_func=None):
-    X_train, y_train = mnist_reader.load_mnist(DATA_DIR, kind='train')
-    X_test, y_test = mnist_reader.load_mnist(DATA_DIR, kind='t10k')
-
-    if transform_func:
-        transform = transform_func(X_train)
-    else:
-        transform = None
-
-    train_loader = DataLoader(dataset=Lenet5_Dataset(X_train, y_train, transform=transform),
-                              batch_size=batch_size,
-                              shuffle=True)
-
-    test_loader = DataLoader(dataset=Lenet5_Dataset(X_test, y_test, transform=transform),
-                              batch_size=batch_size,
-                              shuffle=False)
-
-    return train_loader, test_loader
-
-def normalize(x):
-    x = x / 255.0
-
-    mu = torch.mean(x)
-    sigma = torch.std(x)
-
-    x = transforms.Normalize(mean=mu, std=sigma)(x)
-
-    return x
-
-def permute(x):
-    return x.permute(1, 2, 0)
-
-def transform_func(X):
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(permute),
-        transforms.Lambda(normalize)
-    ])
-
-    return transform
+from data_handler import load_data, transform_func
 
 def fit(model_type, optimizer_method, params):
     weight_decay = params['weight_decay']
@@ -82,7 +41,7 @@ def fit(model_type, optimizer_method, params):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Set Data
-    train_loader, test_loader = _load_data(batch_size, transform_func=transform_func)
+    train_loader, test_loader = load_data(batch_size, transform_func=transform_func)
 
     # Model
     model = Lenet5(model_type=model_type,
@@ -205,17 +164,17 @@ def main():
     models = dict()
     params = {'weight_decay': 0.0,
               'lr': 1.0e-3,
-              'epochs': 15,
+              'epochs': 10,
               'batch_size': 64,
               'lr_scheduler': {
                   'enable': True,
                   'gamma': 0.5,
-                  'step': 10
+                  'step': 3
               }}
 
     for _model_type in MODEL_TYPES:
         if _model_type == 'weight_decay':
-            params['weight_decay'] = 1.0e-2
+            params['weight_decay'] = 1.0e-3
         else:
             params['weight_decay'] = 0.0
 

ex1/model.py
@@ -27,7 +27,7 @@ class Lenet5(nn.Module):
     def _init_model_regular(num_classes):
         layers_list = nn.ModuleList()
 
-        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2),
                                          nn.ReLU(),
                                          nn.MaxPool2d(kernel_size=2, stride=2)))
 
@@ -37,7 +37,10 @@ class Lenet5(nn.Module):
 
         layers_list.append(nn.Flatten())
 
-        layers_list.append(nn.Sequential(nn.Linear(256, 84),
+        layers_list.append(nn.Sequential(nn.Linear(400, 120),
+                                         nn.ReLU()))
+
+        layers_list.append(nn.Sequential(nn.Linear(120, 84),
                                          nn.ReLU()))
 
         layers_list.append(nn.Linear(84, num_classes))
@@ -48,7 +51,7 @@ class Lenet5(nn.Module):
     def _init_model_dropout(num_classes):
         layers_list = nn.ModuleList()
 
-        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2),
                                          nn.ReLU(),
                                          nn.MaxPool2d(kernel_size=2, stride=2)))
 
@@ -58,9 +61,11 @@ class Lenet5(nn.Module):
 
         layers_list.append(nn.Flatten())
 
-        layers_list.append(nn.Sequential(nn.Linear(256, 84),
-                                         nn.ReLU(),
-                                         nn.Dropout(0.5)))
+        layers_list.append(nn.Sequential(nn.Linear(400, 120),
+                                         nn.ReLU()))
+
+        layers_list.append(nn.Sequential(nn.Linear(120, 84),
+                                         nn.ReLU()))
 
         layers_list.append(nn.Linear(84, num_classes))
 
@@ -74,7 +79,7 @@ class Lenet5(nn.Module):
     def _init_model_bn(num_classes):
         layers_list = nn.ModuleList()
 
-        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0),
+        layers_list.append(nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2),
                                          nn.BatchNorm2d(6),
                                          nn.ReLU(),
                                          nn.MaxPool2d(kernel_size=2, stride=2)))
@@ -86,7 +91,10 @@ class Lenet5(nn.Module):
 
         layers_list.append(nn.Flatten())
 
-        layers_list.append(nn.Sequential(nn.Linear(256, 84),
+        layers_list.append(nn.Sequential(nn.Linear(400, 120),
+                                         nn.ReLU()))
+
+        layers_list.append(nn.Sequential(nn.Linear(120, 84),
                                          nn.ReLU()))
 
         layers_list.append(nn.Linear(84, num_classes))